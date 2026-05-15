8f3267d36cf5ea9988ac6aa9f635fe4ec1c6e927
Lester Vilo <lestervilo02@gmail.com>
2026-05-14 22:41:14 -1000

    Add LoopLab application with graphical interface and basic multiplication table

---
+++ Loop/LoopLab.java
@@ -0,0 +1,38 @@
+package Loop;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Graphics;
+
+public class LoopLab extends JPanel {
+    public LoopLab() {
+        // Constructor code here
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Drawing code here
+        int x = 0;
+        int y = 0;
+        var w = getWidth();
+        var h = getHeight();
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        var frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new LoopLab());
+        frame.setVisible(true);
+    }
+    
+}
