04786f2c1bf40fdd43b6facf8bb3454a11cdedcf
Lester Vilo <lestervilo02@gmail.com>
2026-05-24 23:19:06 -1000

    Add MethodsLab class for file input and graphical representation

--- MethodsLab/MethodsLab.java
+++ MethodsLab/MethodsLab.java
@@ -2,8 +2,13 @@ package MethodsLab;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class MethodsLab extends JPanel {
 
@@ -12,10 +17,38 @@ public class MethodsLab extends JPanel {
     public MethodsLab() {
         instructions = new ArrayList<>();
 
+        try {
+            String filename = JOptionPane.showInputDialog("Enter filename:");
+
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+
+        while (input.hasNextLine()) {
+            instructions.add(input.nextLine());
+        }
+
+        input.close();
+
+    } catch (Exception e) {
+        System.out.println("Error reading file: " + e.getMessage());
     }
 
     public void changeColor(int red, int green, int blue, Graphics g) {
-        g.setColor(new java.awt.Color(red, green, blue));
+        Color newColor = new Color(red, green, blue);
+        g.setColor(newColor);
+    }
+
+    public void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, Graphics g) {
+        g.drawLine(x1, y1, x2, y2);
+        g.drawLine(x2, y2, x3, y3);
+        g.drawLine(x3, y3, x1, y1);
+    }
+
+    public void drawCircle(int xc, int yc, int r, Graphics g) {
+        int x = xc - r;
+        int y = yc - r;
+
+        g.drawOval(x, y, r * 2, r * 2);
     }
 
     @Override
@@ -32,3 +65,5 @@ public class MethodsLab extends JPanel {
         window.setVisible(true);
     }
 }
+
+
