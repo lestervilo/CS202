9eecfa12053cac78f92d82129d4bae2e6ac3b414
Lester Vilo <lestervilo02@gmail.com>
2026-06-23 20:26:25 -1000

    Refactor draw method in Room class to use constant SIZE for wall dimensions

--- FinalProject/Room.java
+++ FinalProject/Room.java
@@ -84,30 +84,29 @@ public class Room implements Drawable {
     public void draw(Graphics g) {
         int x = pos.x; // X position of the room
         int y = pos.y; // Y position of the room
-        int size = 50; // Size of the room
         int doorStart = 20; // Starting point of the door opening
         int doorEnd = 30; // Ending point of the door opening
 
         // NORTH
         if (exitNorth == null)
         {
-            g.drawLine(x, y, x + size, y); // Draw the full wall
+            g.drawLine(x, y, x + SIZE, y); // Draw the full wall
         }
         else
         {
             g.drawLine(x, y, x + doorStart, y); // Draw left part of the wall
-            g.drawLine(x + doorEnd, y, x + size, y); // Draw right part of the wall
+            g.drawLine(x + doorEnd, y, x + SIZE, y); // Draw right part of the wall
         }
 
         // SOUTH
         if (exitSouth == null)
         {
-            g.drawLine(x, y + size, x + size, y + size); // Draw the full wall
+            g.drawLine(x, y + SIZE, x + SIZE, y + SIZE); // Draw the full wall
         }
         else
         {
-            g.drawLine(x, y + size, x + doorStart, y + size); // Draw left part of the wall
-            g.drawLine(x + doorEnd, y + size, x + size, y + size); // Draw right part of the wall
+            g.drawLine(x, y + SIZE, x + doorStart, y + SIZE); // Draw left part of the wall
+            g.drawLine(x + doorEnd, y + SIZE, x + SIZE, y + SIZE); // Draw right part of the wall
 
             //Draw the hallway between the rooms
             g.drawLine(x + 20, y + 50, x + 20, y + 60);
@@ -117,23 +116,23 @@ public class Room implements Drawable {
         // WEST
         if (exitWest == null)
         {
-            g.drawLine(x, y, x, y + size); // Draw the full wall
+            g.drawLine(x, y, x, y + SIZE); // Draw the full wall
         }
         else
         {
             g.drawLine(x, y, x, y + doorStart); // Draw upper part of the wall
-            g.drawLine(x, y + doorEnd, x, y + size); // Draw lower part of the wall
+            g.drawLine(x, y + doorEnd, x, y + SIZE); // Draw lower part of the wall
         }
 
         // EAST
         if (exitEast == null)
         {
-            g.drawLine(x + size, y, x + size, y + size); // Draw the full wall
+            g.drawLine(x + SIZE, y, x + SIZE, y + SIZE); // Draw the full wall
         }
         else
         {
-            g.drawLine(x + size, y, x + size, y + doorStart); // Draw upper part of the wall
-            g.drawLine(x + size, y + doorEnd, x + size, y + size); // Draw lower part of the wall
+            g.drawLine(x + SIZE, y, x + SIZE, y + doorStart); // Draw upper part of the wall
+            g.drawLine(x + SIZE, y + doorEnd, x + SIZE, y + SIZE); // Draw lower part of the wall
 
             //Draw the hallway between the rooms
             g.drawLine(x + 50, y + 20, x + 60, y + 20);
